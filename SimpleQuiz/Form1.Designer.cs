e1dc21ecfde6accbce0625f032a0b298a99f054f
M Reza Faisal <rezafaisal@users.noreply.github.com>
2020-03-25 09:38:52 +0800

    Add files via upload

---
+++ SimpleQuiz/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace SimpleQuiz
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButtonMale = new System.Windows.Forms.RadioButton();
+            this.radioButtonFemale = new System.Windows.Forms.RadioButton();
+            this.comboBoxOccupation = new System.Windows.Forms.ComboBox();
+            this.checkBoxCSharp = new System.Windows.Forms.CheckBox();
+            this.checkBoxVBNET = new System.Windows.Forms.CheckBox();
+            this.checkBoxJava = new System.Windows.Forms.CheckBox();
+            this.labelHasil = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(308, 38);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(424, 39);
+            this.textBoxName.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 32);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Gender";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(38, 191);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(136, 32);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Occupation";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(38, 257);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(209, 32);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Programming Skill";
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Location = new System.Drawing.Point(581, 333);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(150, 46);
+            this.buttonSend.TabIndex = 5;
+            this.buttonSend.Text = "Send";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Location = new System.Drawing.Point(408, 333);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(150, 46);
+            this.buttonReset.TabIndex = 6;
+            this.buttonReset.Text = "Reset";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButtonFemale);
+            this.groupBox1.Controls.Add(this.radioButtonMale);
+            this.groupBox1.Location = new System.Drawing.Point(308, 83);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(423, 72);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            // 
+            // radioButtonMale
+            // 
+            this.radioButtonMale.AutoSize = true;
+            this.radioButtonMale.Location = new System.Drawing.Point(6, 30);
+            this.radioButtonMale.Name = "radioButtonMale";
+            this.radioButtonMale.Size = new System.Drawing.Size(98, 36);
+            this.radioButtonMale.TabIndex = 0;
+            this.radioButtonMale.TabStop = true;
+            this.radioButtonMale.Text = "Male";
+            this.radioButtonMale.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonFemale
+            // 
+            this.radioButtonFemale.AutoSize = true;
+            this.radioButtonFemale.Location = new System.Drawing.Point(143, 30);
+            this.radioButtonFemale.Name = "radioButtonFemale";
+            this.radioButtonFemale.Size = new System.Drawing.Size(122, 36);
+            this.radioButtonFemale.TabIndex = 1;
+            this.radioButtonFemale.TabStop = true;
+            this.radioButtonFemale.Text = "Female";
+            this.radioButtonFemale.UseVisualStyleBackColor = true;
+            // 
+            // comboBoxOccupation
+            // 
+            this.comboBoxOccupation.FormattingEnabled = true;
+            this.comboBoxOccupation.Items.AddRange(new object[] {
+            "Government Employee",
+            "Non Government Employee"});
+            this.comboBoxOccupation.Location = new System.Drawing.Point(308, 191);
+            this.comboBoxOccupation.Name = "comboBoxOccupation";
+            this.comboBoxOccupation.Size = new System.Drawing.Size(424, 40);
+            this.comboBoxOccupation.TabIndex = 8;
+            // 
+            // checkBoxCSharp
+            // 
+            this.checkBoxCSharp.AutoSize = true;
+            this.checkBoxCSharp.Location = new System.Drawing.Point(308, 257);
+            this.checkBoxCSharp.Name = "checkBoxCSharp";
+            this.checkBoxCSharp.Size = new System.Drawing.Size(75, 36);
+            this.checkBoxCSharp.TabIndex = 9;
+            this.checkBoxCSharp.Text = "C#";
+            this.checkBoxCSharp.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxVBNET
+            // 
+            this.checkBoxVBNET.AutoSize = true;
+            this.checkBoxVBNET.Location = new System.Drawing.Point(408, 257);
+            this.checkBoxVBNET.Name = "checkBoxVBNET";
+            this.checkBoxVBNET.Size = new System.Drawing.Size(123, 36);
+            this.checkBoxVBNET.TabIndex = 10;
+            this.checkBoxVBNET.Text = "VB.NET";
+            this.checkBoxVBNET.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxJava
+            // 
+            this.checkBoxJava.AutoSize = true;
+            this.checkBoxJava.Location = new System.Drawing.Point(551, 257);
+            this.checkBoxJava.Name = "checkBoxJava";
+            this.checkBoxJava.Size = new System.Drawing.Size(91, 36);
+            this.checkBoxJava.TabIndex = 11;
+            this.checkBoxJava.Text = "Java";
+            this.checkBoxJava.UseVisualStyleBackColor = true;
+            // 
+            // labelHasil
+            // 
+            this.labelHasil.AutoSize = true;
+            this.labelHasil.Location = new System.Drawing.Point(38, 447);
+            this.labelHasil.Name = "labelHasil";
+            this.labelHasil.Size = new System.Drawing.Size(78, 32);
+            this.labelHasil.TabIndex = 12;
+            this.labelHasil.Text = "label5";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 618);
+            this.Controls.Add(this.labelHasil);
+            this.Controls.Add(this.checkBoxJava);
+            this.Controls.Add(this.checkBoxVBNET);
+            this.Controls.Add(this.checkBoxCSharp);
+            this.Controls.Add(this.comboBoxOccupation);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.buttonSend);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Simple Quiz";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.Button buttonReset;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButtonFemale;
+        private System.Windows.Forms.RadioButton radioButtonMale;
+        private System.Windows.Forms.ComboBox comboBoxOccupation;
+        private System.Windows.Forms.CheckBox checkBoxCSharp;
+        private System.Windows.Forms.CheckBox checkBoxVBNET;
+        private System.Windows.Forms.CheckBox checkBoxJava;
+        private System.Windows.Forms.Label labelHasil;
+    }
+}
+
---
+++ SimpleQuiz/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleQuiz
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            labelHasil.Visible = false;
+        }
+
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            string hasil = "";
+            string name = textBoxName.Text;
+            
+            string gender = "";
+            if (radioButtonMale.Checked)
+            {
+                gender = "Male";
+            } else if (radioButtonFemale.Checked)
+            {
+                gender = "Female";
+            } else
+            {
+                MessageBox.Show("Please choose your gender.");
+            }
+
+            string occupation = comboBoxOccupation.Text;
+            if (String.IsNullOrEmpty(occupation))
+            {
+                MessageBox.Show("Please choose your current occupation.");
+            }
+
+            string skill = "";
+            if(checkBoxCSharp.Checked)
+            {
+                skill += "C#, ";
+            }
+
+            if (checkBoxVBNET.Checked)
+            {
+                skill += "VB, ";
+            }
+
+            if (checkBoxJava.Checked)
+            {
+                skill += "Java";
+            }
+
+            if (String.IsNullOrEmpty(skill))
+            {
+                MessageBox.Show("Please choose your programming skill.");
+            }
+            
+            if(!String.IsNullOrEmpty(name) || !String.IsNullOrEmpty(gender) || !String.IsNullOrEmpty(occupation) || !String.IsNullOrEmpty(skill))
+            {
+                hasil = "Name: " + name + "\n";
+                hasil += "Gender: " + gender + "\n";
+                hasil += "Occupation: " + occupation + "\n";
+                hasil += "Programming Skills: " + skill + "\n";
+
+                labelHasil.Visible = true;
+                labelHasil.Text = hasil;
+            }
+        }
+
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+            textBoxName.Text = "";
+            radioButtonMale.Checked = false;
+            radioButtonFemale.Checked = false;
+            comboBoxOccupation.SelectedIndex = -1;
+            checkBoxCSharp.Checked = false;
+            checkBoxVBNET.Checked = false;
+            checkBoxJava.Checked = false;
+            labelHasil.Visible = false;
+        }
+    }
+}
